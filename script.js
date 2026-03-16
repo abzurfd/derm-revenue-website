3cac5454af27d957b61b7ee16e95d650cdb4f902
abzurfd <89162715+abzurfd@users.noreply.github.com>
2026-03-15 23:33:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function saveRequest(data) {
 document.querySelectorAll(".platform-download").forEach((card) => {
   const platform = card.getAttribute("data-platform");
   if (platform === "mac") {
-    card.setAttribute("href", "https://drive.google.com/uc?export=download&id=1qmza7laKTSsVEEFeanH7W56oDEDymrni");
+    card.setAttribute("href", "https://www.dropbox.com/scl/fi/keq7si62h8vjvwujsce8o/Health-Solutions-mac.zip?rlkey=6azi244qaf32ucyso50e9yo3w&st=si8fulm1&dl=1");
     card.setAttribute("download", "Health-Solutions-mac.zip");
     return;
   }
